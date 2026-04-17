94892b304d130e498b7d47a0924132efed672313
geooeg <kgeo6264@gmail.com>
2026-04-17 10:48:32 +0900

    feat: Add data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
-INSERT INTO employee (name, department, salary) VALUES ('김지오', '개발팀', 50000000);
-INSERT INTO employee (name, department, salary) VALUES ('이지오', '개발팀', 55000000);
-INSERT INTO employee (name, department, salary) VALUES ('박지오', '마케팅팀', 45000000);
-INSERT INTO employee (name, department, salary) VALUES ('정지오', '마케팅팀', 48000000);
-INSERT INTO employee (name, department, salary) VALUES ('유지오', '인사팀', 42000000);
+INSERT INTO employee (name, department, salary) VALUES ('김지오', '개발팀', 5000000000);
+INSERT INTO employee (name, department, salary) VALUES ('이지오', '개발팀', 5500000000);
+INSERT INTO employee (name, department, salary) VALUES ('박지오', '마케팅팀', 4500000000);
+INSERT INTO employee (name, department, salary) VALUES ('정지오', '마케팅팀', 4800000000);
+INSERT INTO employee (name, department, salary) VALUES ('유지오', '인사팀', 4200000000);
